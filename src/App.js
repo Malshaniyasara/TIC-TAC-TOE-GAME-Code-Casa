363c3301a4daee7522e20e6593fc14bdf02f05c7
yasara malshani <137792898+Malshaniyasara@users.noreply.github.com>
2023-10-29 11:43:06 +0530

    react framework

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import square from './square';
+import Board from './Board';
+
+function App() {
+  return (
+    <div className="App">
+<square/>
+    </div>
+  );
+}
+
+export default App;
